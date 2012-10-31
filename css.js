49a0f808727bc004a4de7ba2fa36e3a680db86ca
kui.zheng <kuii.zheng@gmail.com>
2012-10-31 11:25:18 +0800

    css: add filters benchmarks: blur/brightness/dropshadow      add CSS feature based benchmarks: border_image/border_radius/box_shadow

---
+++ css.js
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2012 ARM Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY UNIVERSITY OF SZEGED ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL UNIVERSITY OF SZEGED OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+var methanol_tests = Array(
+/*
+ * feature based benchmarks.
+ */
+    "css/feature_based/css_border_image.html",
+    "css/feature_based/css_border_radius.html",
+    "css/feature_based/css_box_shadow.html",
+/*
+ * CSS filters benchmarks.
+ */
+    "css/css_filters/css_filter_dropshadow.html",
+    "css/css_filters/css_filter_blur.html",
+    "css/css_filters/css_filter_brightness.html"
+);
+
