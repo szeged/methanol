907c82d96a173550533d45abf9622cf8d1bbfd99
Zheng Xu <Zheng.Xu@arm.com>
2013-06-14 14:04:58 +0800

    add Utils object to provides functions required by Canvas2D tests. add missing <html> tag.

--- canvas/engines/utils.js
+++ canvas/engines/utils.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2012 ARM Inc.
+ * Copyright (C) 2012-2013 Arm Limited
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -11,13 +11,13 @@
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
  *
- * THIS SOFTWARE IS PROVIDED BY UNIVERSITY OF SZEGED ``AS IS'' AND ANY
- * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL UNIVERSITY OF SZEGED OR
- * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * THIS SOFTWARE IS PROVIDED BY UNIVERSITY OF SZEGED AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL UNIVERSITY OF
+ * SZEGED OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
  * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
  * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
@@ -44,19 +44,36 @@ window.cancelRequestAnimFrame = (function () {
            window.clearTimeout;
 })();
 
-function getRandom(min, max) {
-    switch (arguments.length) {
-        case 1: return parseInt(Math.random() * min + 1);
-        case 2: return parseInt(Math.random() * (max - min + 1) + min);
-        default: return 0;
-    }
-}
+var Utils = {
+    getRandom: function (min, max) {
+        switch (arguments.length) {
+            case 1: return parseInt(Math.random() * min + 1);
+            case 2: return parseInt(Math.random() * (max - min + 1) + min);
+            default: return 0;
+        }
+    },
+    loadImg: function (src) {
+        var img = document.createElement("img");
+        img.src = src;
+        return img;
+    },
+    getRandomColor: function() {
+        var colors = [
+            "#000000", "#FFFFFF",
+            "#FF0000", "#00FF00", "#0000FF",
+            "#00FFFF", "#FF00FF", "#FFFF00",
+        ];
+        var index = Math.floor(Math.random() * colors.length);
+        return colors[index];
+    },
+};
 
-function MethanolCanvas2DTest(redraw) {
+function MethanolCanvas2DTest(redraw, initDraw) {
     var frameCount = 0;
     var startTime = Date.now();
     var ctx;
-    var drawCtx = redraw;
+    var drawCtx = typeof (redraw) == "function" ? redraw : function () { };
+    var drawInit = typeof (initDraw) == "function" ? initDraw : function () { };
 
     function animationFrameCallback() {
         frameCount++;
@@ -76,6 +93,7 @@ function MethanolCanvas2DTest(redraw) {
         document.body.innerHTML = '<canvas id="canvas" width="800" height="600" />';
         var canvas = document.getElementById('canvas');
         ctx = canvas.getContext('2d');
+        drawInit(ctx);
         window.requestAnimFrame(animationFrameCallback);
     }
 
