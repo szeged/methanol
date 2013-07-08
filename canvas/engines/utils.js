db02436b6d399d980cf6754a52d79e072e402dcc
Zheng Xu <Zheng.Xu@arm.com>
2013-07-08 17:31:20 +0800

    add Canvas2D case :   draw convex quadratic bezier curves   draw concave quadratic bezier curves   draw rectangle with gradient fill style   draw rectangle with shadow   draw patterned rectangle   draw image   image data     this case mainly measure the performance of accessing pixel data, not get/put image data
    enable newly added test cases.
      replace new Array() with [] to create array object, so that we can even add comma at the last cases. This will be convenient for us to debug test cases.
    
    add loadImg() and getRandomColor() to Utils object which are commonly used by several test cases.

--- canvas/engines/utils.js
+++ canvas/engines/utils.js
@@ -52,6 +52,15 @@ var Utils = {
             default: return 0;
         }
     },
+    loadImg: function (src) {
+        var img = document.createElement("img");
+        img.src = src;
+        return img;
+    },
+    getRandomColor: function () {
+        var color = "#" + ((Math.random() * (1 << 24)) | (1 << 24)).toString(16).substr(1);
+        return color;
+    },
 };
 
 function MethanolCanvas2DTest(redraw, initDraw) {
