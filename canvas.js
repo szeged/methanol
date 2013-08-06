797f9aae6cedac600514bc02b638e0a880d649e5
Zheng Xu <Zheng.Xu@arm.com>
2013-08-06 10:54:15 +0800

    add test cases including: filled & stroked arc, image, line with different caps, join and stroke operations, compositing operations, text and transformation.

--- canvas.js
+++ canvas.js
@@ -35,5 +35,43 @@ var methanol_tests = [
     "canvas/feature_based/canvas_rect_gradient.html",
     "canvas/feature_based/canvas_rect_pattern.html",
     "canvas/feature_based/canvas_rect_shadow.html",
+/*
+ * curve
+ *
+ */
+    "canvas/feature_based/canvas_curve_archimedean_circle.html",
+    "canvas/feature_based/canvas_curve_cissoid_circle.html",
+    "canvas/feature_based/canvas_curve_conchoid_circle.html",
+    "canvas/feature_based/canvas_curve_lissajous_grad_circle.html",
+    "canvas/feature_based/canvas_curve_lituus_circle.html",
+    "canvas/feature_based/canvas_curve_sine_grad_double_circle.html",
+/*
+ * compositing
+ *
+ */
+    "canvas/feature_based/canvas_comp_copy.html",
+    "canvas/feature_based/canvas_comp_des_atop.html",
+    "canvas/feature_based/canvas_comp_des_in.html",
+    "canvas/feature_based/canvas_comp_des_out.html",
+    "canvas/feature_based/canvas_comp_des_over.html",
+    "canvas/feature_based/canvas_comp_lighter.html",
+    "canvas/feature_based/canvas_comp_src_atop.html",
+    "canvas/feature_based/canvas_comp_src_in.html",
+    "canvas/feature_based/canvas_comp_src_out.html",
+    "canvas/feature_based/canvas_comp_src_over.html",
+    "canvas/feature_based/canvas_comp_xor.html",
+
+    "canvas/feature_based/canvas_draw_arc_fill.html",
+    "canvas/feature_based/canvas_draw_arc_stroke.html",
+    "canvas/feature_based/canvas_draw_image_alone.html",
+    "canvas/feature_based/canvas_draw_line_cap.html",
+    "canvas/feature_based/canvas_draw_line_join.html",
+    "canvas/feature_based/canvas_draw_line_stroke_grad.html",
+    "canvas/feature_based/canvas_draw_line_stroke.html",
+    "canvas/feature_based/canvas_draw_line_stroke_pattern.html",
+    "canvas/feature_based/canvas_gradient_rad.html",
+    "canvas/feature_based/canvas_gradient_rad_rect.html",
+    "canvas/feature_based/canvas_text.html",
+    "canvas/feature_based/canvas_transformation.html",
 ];
 
